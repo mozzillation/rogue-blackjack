87f1fb8830870c3f7de6f4ad05c771c94bcb0e8d
Giuliano Mozzillo <giulianomozzillo@live.it>
2025-03-17 10:52:01 +0100

    🚀 Metadata

--- src/app/[seed]/page.tsx
+++ src/app/[seed]/page.tsx
@@ -4,6 +4,12 @@ type SeedPageProps = {
     params: Promise<{ seed: string }>
 }
 
+export const generateMetadata = async ({ params }: SeedPageProps) => {
+    return {
+        title: `Seed: ${(await params).seed}`,
+    }
+}
+
 const SeedPage: React.FC<SeedPageProps> = async ({ params }) => {
     const { seed } = await params
 
